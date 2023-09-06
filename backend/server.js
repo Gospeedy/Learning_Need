0bfb43448b1db7950e26eb4a0a243c09b3be21d2
Pratip <pratipkayal@optimasedge.com>
2023-09-06 16:54:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDB = require("./db/connectDB")
 const cloudinary = require("cloudinary");
+const path = require('path')
 
 // Handling Uncaught Execption => anything not defind Uncaught Execption 
 
@@ -24,6 +25,13 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
+});
+
+
+app.use(express.static(path.join(__dirname, "../frotend/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frotend/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
